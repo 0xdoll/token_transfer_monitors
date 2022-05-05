6dfe39f62b37f8fad7280f395ed472ad17132786
0xdoll <98440810+0xdoll@users.noreply.github.com>
2022-05-05 13:56:15 +0800

    fix solana USDT token address

--- solana/main.go
+++ solana/main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	// TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA: Solana USDT address
+	// Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB // Solana USDT address
 	um_confirmed := lib.TokenTransferMonitor{COMMITMENT: "confirmed", MonitorTokenAddress: "Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB"}
 	um_confirmed.Monitor()
 }
